b7f7ab6d65697d8ba3bcdcb20914a4a1a188be41
Nicolas Misseri <nicomisseri1@gmail.com>
2021-05-13 15:05:25 -0300

    Se agrego el boton Agregar Salario y su calculo

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -15,57 +15,66 @@ const cantidadDeIntegrantes = Number(
 
 const div = document.querySelector("div");
 
-function crearLabelEdad() {
+function crearLabelEdad(divIntegrante, index) {
   let label = document.createElement("label");
-  div.appendChild(label).textContent = "Edad del integrante: ";
+  divIntegrante.appendChild(label).textContent = "Edad del integrante: ";
 }
 
-function crearInputEdad(index) {
+function crearInputEdad(divIntegrante, index) {
   let input = document.createElement("input");
-  let inputNuevo = div.appendChild(input);
+  let inputNuevo = divIntegrante.appendChild(input);
   inputNuevo.type = "number";
   inputNuevo.id = `edad${index}`;
 }
 
-function crearLabelSalario() {
+function crearLabelSalario(divIntegrante, index) {
   let label = document.createElement("label");
-  div.appendChild(label).textContent = "Salario del integrante: ";
+  divIntegrante.appendChild(label).textContent = "Salario del integrante: ";
 }
 
-function crearInputSalario(index) {
+function crearInputSalario(divIntegrante, index) {
   let input = document.createElement("input");
-  let inputNuevo = div.appendChild(input);
+  let inputNuevo = divIntegrante.appendChild(input);
   inputNuevo.type = "number";
   inputNuevo.id = `salario${index}`;
 }
 
-function crearBr() {
+function crearBr(divIntegrante, index) {
   let br = document.createElement("br");
-  div.appendChild(br);
+  divIntegrante.appendChild(br);
 }
 
-function crearBotonAgregarSalario() {
+function botonAgregarSalario(divIntegrante, index) {
   let $botonSalario = document.createElement("button");
-  div.appendChild($botonSalario);
+  divIntegrante.appendChild($botonSalario);
   $botonSalario.textContent = "Agregar Salario";
   $botonSalario.onclick = () => {
-    crearLabelSalario();
-    crearInputSalario(index);
-    crearBr();
-    crearBr();
+    crearLabelSalario(divIntegrante, index);
+    crearInputSalario(divIntegrante, index);
+    crearBr(divIntegrante, index);
+    crearBr(divIntegrante, index);
 
     return false;
   };
 }
 
+// Crear un integrante de la familia
+
+function crearIntegrante(index) {
+  let divIntegrante = document.createElement("div");
+  divIntegrante.id = `integrante${index}`;
+  div.appendChild(divIntegrante);
+
+  crearLabelEdad(divIntegrante, index);
+  crearInputEdad(divIntegrante, index);
+  botonAgregarSalario(divIntegrante, index);
+  crearBr(divIntegrante, index);
+  crearBr(divIntegrante, index);
+}
 // Crear la cantidad correspondiente a la cantidad de integrantes
 
 for (let index = 0; index < cantidadDeIntegrantes; index++) {
-  crearLabelEdad();
-  crearInputEdad(index);
-  crearBotonAgregarSalario();
-  crearBr();
-  crearBr();
+  crearIntegrante(index);
 }
 // Traer los em para modificar
 
@@ -75,42 +84,76 @@ const $resultadoSalarios = document.querySelector("#resultadoSalarios");
 // Calcular las edades
 
 function CalcularMenorMayorPromedioYMostrar() {
-  const array = [];
+  // CREO LOS ARRAYS
+  const arrayEdades = [];
+  const arraySalarios = [];
+
   //   Inicializo totales en 0
   let totalEdades = 0;
   let totalSalarios = 0;
 
-  // Calcular los totales
+  // Calcular los totales Y relleno los arrays
+
   for (let index = 0; index < cantidadDeIntegrantes; index++) {
-    array.push(Number(document.querySelector(`#edad${index}`).value));
+    arrayEdades.push(Number(document.querySelector(`#edad${index}`).value));
     totalEdades += Number(document.querySelector(`#edad${index}`).value);
-    // totalSalarios += Number(document.querySelector(`#salario${index}`).value);
+
+    arraySalarios.push(
+      Number(document.querySelector(`#salario${index}`).value)
+    );
+    totalSalarios += Number(document.querySelector(`#salario${index}`).value);
   }
-
-  //   Buscar el mayor
-
-  for (let index = 0; index < cantidadDeIntegrantes; index++) {
-    maximo = Math.max(array[index]);
-    minimo = Math.min(array[index]);
-  }
-  let $edadMaxima = document.querySelector("#edadMaxima");
-  $edadMaxima.textContent = `La edad maxima de la familia es: ${maximo}`;
-
-  let $edadMinima = document.querySelector("#edadMinima");
-  $edadMinima.textContent = `La edad minima de la familia es: ${minimo}`;
-
-  //   Mostrar el promedio dividiendo por la cantidad de integrantes
-
   $resultadoEdades.textContent = `El promedio de edad de los integrantes es de : ${
     totalEdades / cantidadDeIntegrantes
   }`;
-
   $resultadoSalarios.textContent = `El promedio de salario de los integrantes es de : ${
     totalSalarios / cantidadDeIntegrantes
   }`;
+
+  //   Buscar el mayor y el menor
+
+  //   EDADES
+
+  for (let index = 0; index < cantidadDeIntegrantes; index++) {
+    let minimoEdades = 99999999;
+    let maximoEdades = -1;
+    for (let index = 0; index < arrayEdades.length; index++) {
+      if (arrayEdades[index] < minimoEdades) {
+        minimoEdades = arrayEdades[index];
+      }
+      if (arrayEdades[index] > maximoEdades) {
+        maximoEdades = arrayEdades[index];
+      }
+    }
+    let $edadMaxima = document.querySelector("#edadMaxima");
+    $edadMaxima.textContent = `La edad maxima de la familia es: ${maximoEdades}`;
+
+    let $edadMinima = document.querySelector("#edadMinima");
+    $edadMinima.textContent = `La edad minima de la familia es: ${minimoEdades}`;
+  }
+
+  //   SALARIOS
+
+  for (let index = 0; index < cantidadDeIntegrantes; index++) {
+    let minimoSalarios = 99999999;
+    let maximoSalarios = -1;
+    for (let index = 0; index < arraySalarios.length; index++) {
+      if (arraySalarios[index] < minimoSalarios) {
+        minimoSalarios = arraySalarios[index];
+      }
+      if (arraySalarios[index] > maximoSalarios) {
+        maximoSalarios = arraySalarios[index];
+      }
+    }
+    let $edadMaxima = document.querySelector("#salarioMaximo");
+    $edadMaxima.textContent = `El salario maximo de la familia es: ${maximoSalarios}`;
+
+    let $edadMinima = document.querySelector("#salarioMinimo");
+    $edadMinima.textContent = `El salario minimo de la familia es: ${minimoSalarios}`;
+  }
 }
 
-// Crear Botones
+// CREAR BOTONES
 
 // Boton Calcular
 
